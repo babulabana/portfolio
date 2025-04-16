c88deed4e17c42fdb6f5da25e504c043319d25ed
Babu labana <115482429+babulabana@users.noreply.github.com>
2025-04-16 22:40:36 +0530

    yt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,9 +11,8 @@ export default function About() {
           <div className="my-10">
             <p>
               Hi Everyone, I am
-              <span className="text-fuchsia-700">Vishal Labana</span> from
-              <span className="text-fuchsia-700">
-                District Mahisagar, Gujarat, India.
+              <span className="text-fuchsia-700">Vishal Labana</span> from 
+              <span className="text-fuchsia-700">    District Mahisagar, Gujarat, India.
               </span>
             </p>
             <p>
